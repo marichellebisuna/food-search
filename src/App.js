0a7d1ec20f3223bd681edee2131067d4ace67bb7
Marichelle Bisuna <marichellebisuna@yahoo.com>
2019-02-11 11:10:02 +1100

    connecting API from FoodtoFork

--- src/App.js
+++ src/App.js
@@ -6,7 +6,29 @@ import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 class App extends Component {
+  state = {
+    recipes:[],
+    url:"https://www.food2fork.com/api/search?key=d3187323b2f39c6c707dd4b6f2ee98be"
+  };
+  async getRecipes(){
+    try{
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      this.setState({
+        recipes:jsonData.recipes
+      });
+    }
+    catch(error){
+      console.log(error);
+    }
+  };
+
+  componentDidMount(){
+    this.getRecipes()
+  };
+
   render() {
+    console.log(this.state.recipes);
     return (
       <React.Fragment>
         <RecipeList />
